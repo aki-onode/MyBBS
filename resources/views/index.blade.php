ac2993a73ddd2c5d66acb32f099361ddb9502ab0
Akito Onodera <>
2023-01-14 08:58:05 +0900

    #08 投稿一覧ページを作ろう

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MyBBS</title>
+</head>
+
+<body>
+    <h1>My BBS</h1>
+    <ul>
+        <li>Title</li>
+        <li>Title</li>
+        <li>Title</li>
+    </ul>
+</body>
+
+</html>
