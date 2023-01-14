699666baa569f31a721811fcfcb3cbe5359c3078
Akito Onodera <>
2023-01-14 11:57:03 +0900

    #20 コンポーネントを使ってみよう

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+l lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MyBBS</title>
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+</head>
+
+<body>
+    <div class="container">
+        {{ $slot }}
+    </div>
+
+</body>
+
+</html>
