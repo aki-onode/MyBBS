a39cc94d83021e72c7ea9b13ab3fc23866a8cded
Akito Onodera <>
2023-01-16 21:03:29 +0900

    #13 編集フォームのViewを作ろう

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,18 +1,18 @@
 <x-layout>
     <x-slot name="title">
-        Add New Post - My BBS
+        Edit Post - My BBS
     </x-slot>
     <div class="back-link">
-        &laquo; <a href="{{ route('posts.index') }}">Back</a>
+        &laquo; <a href="{{ route('posts.show', $post) }}">Back</a>
     </div>
-    <h1>Add New Post</h1>
+    <h1>Edit Post</h1>
 
-    <form method="post" action="{{ route('posts.store') }}">
+    <form method="post" action="">
         @csrf
         <div class="form-group">
             <label>
                 Title
-                <input type="text" name="title" value="{{ old('title') }}">
+                <input type="text" name="title" value="{{ old('title', $post->title) }}">
             </label>
             @error('title')
                 <div class="error">{{ $message }}</div>
@@ -21,16 +21,14 @@
         <div class="form-group">
             <label>
                 Body
-                <textarea name="body">{{ old('body') }}</textarea>
+                <textarea name="body">{{ old('body', $post->body) }}</textarea>
             </label>
             @error('body')
                 <div class="error">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-button">
-            <button>Add</button>
+            <button>Update</button>
         </div>
-
-
     </form>
 </x-layout>
