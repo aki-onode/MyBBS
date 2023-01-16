8530bfabd4a690335ffea97383656e3bd22889b7
Akito Onodera <>
2023-01-16 20:57:51 +0900

    #12 投稿の編集機能を実装しよう

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/posts/create', [PostController::class, 'create'])
     ->name('posts.create');
 Route::post('/posts/store', [PostController::class, 'store'])
     ->name('posts.store');
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])
+    ->name('posts.edit')
+    ->where('post', '[0-9]+');
